d475a1e2d1f38e236c9aaa6b92066d369644a79c
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-01-19 20:51:46 +0530

    seperated bottom nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,8 @@ import { Link } from "react-router-dom";
 import GithubSVG from "./GithubSVG";
 import LinkedInSVG from "./LinkedInSVG";
 import { useState } from "react";
-import HomeSVG from "./HomeSVG";
-import IdeaSVG from "./IdeaSVG";
-import PersonSVG from "./PersonSVG";
-import PhoneSVG from "./PhoneSVG";
+
+import BottomNav from "./BottomNav";
 const Navbar = () => {
   const [theme, setTheme] = useState("light");
 
@@ -27,14 +25,14 @@ const Navbar = () => {
   }
 
   return (<>
-    <header className="sticky h-20 md:h-28 top-0 z-20 flex pt-4 w-full backdrop-blur">
+    <header className="sticky h-16 md:h-28 top-0 z-20 flex pt-[4px] w-full backdrop-blur">
       <div className="mx-auto w-full max-w-6xl ">
         {/* for now addded overflow scroll to nav need to remove and convert it to sidebar */}
         <nav className="flex items-center md:gap-6 gap-3 md:justify-normal justify-between">
           {/* <!-- Brand logo --> */}
           <div className="logo inline-flex shrink-0 md:ml-0 ml-3">
             <Link to={`/`}>
-              <img src={brandLogo} className="rounded-full size-16 md:size-20" alt="brand logo" />
+              <img src={brandLogo} className="rounded-full size-14 md:size-20" alt="brand logo" />
             </Link>
           </div>
           {/* <!-- Brand logo --> */}
@@ -58,19 +56,19 @@ const Navbar = () => {
               handleChangeTheme("dark");
 
 
-            }}><img src={moonSVG} width={"30px"} alt="moon SVG" /> </button>}
+            }}><img src={moonSVG} className="md:w-[30px] w-[20px]" alt="moon SVG" /> </button>}
 
             {theme === "dark" && <button className="flex-shrink-0" onClick={() => {
               handleChangeTheme("light");
 
 
-            }}><img src={sunSVG} width={"30px"} alt="moon SVG" /> </button>}
+            }}><img src={sunSVG} className="md:w-[30px] w-[25px]" alt="moon SVG" /> </button>}
 
             <a href="https://github.com/randhavevaibhav" className="inline-flex shrink-0" target="_blank">
-              <GithubSVG />
+              <GithubSVG width="25px" />
             </a>
             <a href="https://www.linkedin.com/in/vaibhav-randhave-b8b099205/" className="inline-flex shrink-0 md:ml-0 mr-3" target="_blank">
-              <LinkedInSVG />
+              <LinkedInSVG width="25px" />
             </a>
 
             {/* <!-- Social media icons --> */}
@@ -80,26 +78,7 @@ const Navbar = () => {
     </header>
 
 
-    <div className="md:hidden h-[50px] items-center md:gap-6 gap-4 flex fixed bottom-0 w-full justify-around backdrop-blur">
-      <Link to={"/"} className="text-xs text-primary font-bold flex flex-col items-center">
-        <span><HomeSVG /></span>
-        <span>Home</span>
-      </Link>
-      <a href="#projects" className="text-xs text-primary font-bold flex flex-col items-center">
-        <span><IdeaSVG /></span>
-        <span>Projects</span>
-      </a>
-      <Link to={"/about"} className="text-xs font-bold text-primary flex flex-col items-center">
-        <span><PersonSVG /></span>
-
-        <span>About</span>
-      </Link>
-      <a href="#" className="text-xs font-bold text-primary flex flex-col items-center">
-        <span><PhoneSVG /></span>
-        <span>Contact</span>
-
-      </a>
-    </div>
+    <BottomNav />
 
 
   </>
