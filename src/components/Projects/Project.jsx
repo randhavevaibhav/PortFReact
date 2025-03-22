fb28b3afdce5584d931467365918a46126d6c741
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-03-22 23:32:59 +0530

    updated img styles

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export const Project = ({ projectId,projectImg, projectName, summary }) => {
+export const Project = ({ projectId, projectImg, projectName, summary }) => {
   return (
     <>
-      <Link to={`/projects/${projectId}`} className="border border-themeborder rounded-xl">
+      <Link
+        to={`/projects/${projectId}`}
+        className="border border-themeborder rounded-xl"
+      >
         <div className="rounded-t-[inherit]">
           <img
             src={projectImg}
             alt="project 1 image"
-            className="w-[32rem] rounded-[inherit]"
+            className="w-[32rem] rounded-[inherit] object-cover"
           />
         </div>
         <div className="flex flex-col gap-2 p-5">
-          <h1 className="md:text-3xl text-2xl font-bold mb-3 capitalize">{projectName}</h1>
-          <p  className="text-sm md:text-base">{summary}</p>
+          <h1 className="md:text-3xl text-2xl font-bold mb-3 capitalize">
+            {projectName}
+          </h1>
+          <p className="text-sm md:text-base">{summary}</p>
         </div>
       </Link>
     </>
